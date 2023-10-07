3c9491557e4e525fee19fb6124d1ca6e2b149326
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2023-10-07 11:49:41 +1000

    ♻️ Gateway cals UI & API

---
+++ src/TimesheetGPT.Gateway/Program.cs
@@ -0,0 +1,20 @@
+using TimesheetGPT.WebAPI;
+using TimesheetGPT.WebUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddUserSecrets(typeof(Program).Assembly);
+
+builder.Services.AddTimesheetGptUi(builder.Configuration);
+/*
+builder.Services.AddTimesheetGptApi(builder.Configuration);
+*/
+
+var app = builder.Build();
+
+/*
+app.UseTimesheetGptApi();
+*/
+app.UseTimesheetGptUi();
+
+app.Run();
